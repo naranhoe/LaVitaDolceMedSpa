713f74c77c5de366952bafd9a1a7506aec89a280
Jorge Naranjo <j.naranjojr@gmail.com>
2016-04-19 18:54:12 -0400

    Add working social media links. Still having trouble with sending email when a user fills out the form.

--- contact.php
+++ contact.php
@@ -1,6 +1,6 @@
 <?php
 
-//Retrieve form data. 
+//Retrieve form data.
 //GET - user submitted data using AJAX
 //POST - in case user does not support javascript, we'll use POST instead
 $name = ($_GET['name']) ? $_GET['name'] : $_POST['name'];
@@ -13,36 +13,36 @@ if ($_POST) $post=1;
 
 //Simple server side validation for POST data, of course, you should validate the email
 if (!$name) $errors[count($errors)] = 'Please enter your name.';
-if (!$email) $errors[count($errors)] = 'Please enter your email.'; 
-if (!$comment) $errors[count($errors)] = 'Please enter your message.'; 
+if (!$email) $errors[count($errors)] = 'Please enter your email.';
+if (!$comment) $errors[count($errors)] = 'Please enter your message.';
 
 //if the errors array is empty, send the mail
 if (!$errors) {
 
 	//recipient - replace your email here
-	$to = 'wowthemesnet@gmail.com';	
+	$to = 'j.naranjojr@gmail.com';
 	//sender - from the form
 	$from = $name . ' <' . $email . '>';
-	
+
 	//subject and the html message
-	$subject = 'Message from ' . $name;	
+	$subject = 'Message from ' . $name;
 	$message = 'Name: ' . $name . '<br/><br/>
-		       Email: ' . $email . '<br/><br/>		
+		       Email: ' . $email . '<br/><br/>
 		       Message: ' . nl2br($comment) . '<br/>';
 
 	//send the mail
 	$result = sendmail($to, $subject, $message, $from);
-	
+
 	//if POST was used, display the message straight away
 	if ($_POST) {
 		if ($result) echo 'Thank you! We have received your message.';
 		else echo 'Sorry, unexpected error. Please try again later';
-		
-	//else if GET was used, return the boolean value so that 
+
+	//else if GET was used, return the boolean value so that
 	//ajax script can react accordingly
 	//1 means success, 0 means failed
 	} else {
-		echo $result;	
+		echo $result;
 	}
 
 //if the errors array has values
@@ -53,15 +53,14 @@ if (!$errors) {
 	exit;
 }
 
-
 //Simple mail function with HTML header
 function sendmail($to, $subject, $message, $from) {
 	$headers = "MIME-Version: 1.0" . "\r\n";
 	$headers .= "Content-type:text/html;charset=iso-8859-1" . "\r\n";
 	$headers .= 'From: ' . $from . "\r\n";
-	
+
 	$result = mail($to,$subject,$message,$headers);
-	
+
 	if ($result) return 1;
 	else return 0;
 }
